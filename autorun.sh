b86d0e6399faf4c15864a69c7eab49faf535beff
vijay6672 <vijayelectro2002@gmail.com>
2021-09-10 11:20:38 +0530

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -2,10 +2,10 @@
 
 echo $(dirname $0)
 
-python3 -m pip install requests
+python -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 youtube_m3ugrabber.py > ../youtube.m3u
+python youtube_m3ugrabber.py > ../youtube.m3u
 
 echo m3u grabbed
